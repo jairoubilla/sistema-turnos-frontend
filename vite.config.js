614ce41daa6aef3f3518c9a477fbc46538d0e0e4
Jairo <jwu.monk@gmail.com>
2026-01-16 10:32:44 -0300

    Permitir host de Railway en Vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'sistema-turnos-frontend-production.up.railway.app'
+    ],
+    host: true,
+    port: 4173
+  }
 })
